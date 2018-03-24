0180f9f4011b77051855bc44008fb5e96ca1f178
Saylee <dharnesk@mail.uc.edu>
2018-03-24 13:14:37 -0400

    cleaning

--- src/reporting_developer_ui.py
+++ src/reporting_developer_ui.py
@@ -23,25 +23,25 @@ class ConfigInterface:
     
     def __init__(self, server='SAYLEE-PC\MSSQLSERVER01'):
         
-        server = 'SAYLEE-PC\MSSQLSERVER01' 
+        self.server = server
         database = 'Reporting_developer' 
-        cnxn = pyodbc.connect(
-        r'DRIVER={ODBC Driver 13 for SQL Server};'
-        r'SERVER='+server+';'
-        r'DATABASE='+database+';'
-        r'Trusted_Connection=yes;'
-        r'QuotedID=Yes;'
-        r'AnsiNPW=Yes;'
-        )
-        self.cursor = cnxn.cursor()
+        connection = pyodbc.connect(
+            r'DRIVER={ODBC Driver 13 for SQL Server};'
+            r'SERVER='+server+';'
+            r'DATABASE='+database+';'
+            r'Trusted_Connection=yes;'
+            r'QuotedID=Yes;'
+            r'AnsiNPW=Yes;'
+            )
+        self.cursor = connection.cursor()
     
     
 class ReportingDeveloperUI:
     def __init__(self):
-        self.interface = self.configureConnectionForm()
-        
+        # self.interface = self.configureConnectionForm()
+        print("Placeholder")
     
-    def configureConnectionForm(self):
+    def configure_connection_form(self):
         print("")
     
         
@@ -56,19 +56,15 @@ class ReportingDeveloperProcedures:
         for row in self.cursor.columns(table=tablename):
             print (row.column_name)
 
-            
-
     def add_business_review(self, item_id, approval, reviewer, comment, reviewed_datetime = datetime.datetime.now()):
         reviewed = reviewed_datetime.strftime('%Y-%m-%d %H:%M:%S')
-        self.cursor.execute("insert into work.business_reviews (item_id, approval,reviewer,comment,reviewed)values(?,?,?,?,?)", 
+        self.cursor.execute("insert into work.business_reviews (item_id, approval,reviewer,comment,reviewed)values(?,?,?,?,?)",
             (item_id, approval, reviewer, comment, reviewed))
     
     def add_peer_review(self, item_id, approval, comment, reviewer= os.getlogin(), reviewed_datetime = datetime.datetime.now()):
         reviewed = reviewed_datetime.strftime('%Y-%m-%d %H:%M:%S')
         self.cursor.execute("insert into work.peer_reviews (item_id, approval,reviewer,comment,reviewed)values(?,?,?,?,?)", 
             (item_id, approval, reviewer, comment, reviewed))
-
-    def update_status(self):
 
     def add_request_review(self, item_id, approval, comment, reviewer= os.getlogin(), reviewed_datetime = datetime.datetime.now()):
         reviewed = reviewed_datetime.strftime('%Y-%m-%d %H:%M:%S')
@@ -81,13 +77,12 @@ class ReportingDeveloperProcedures:
 
     def add_level_of_effort(self, item_id, estimate, developer= os.getlogin(), added_datetime=datetime.datetime.now()):
         added = added_datetime.strftime('%Y-%m-%d %H:%M:%S')
-        self.cursor.execute("insert into work.level_of_effort (item_id, estimate,develeoper, added)values(?,?,?,?)", 
+        self.cursor.execute("insert into work.level_of_effort (item_id, estimate,developer, added)values(?,?,?,?)",
             (item_id, estimate, developer, added))
 
     def add_developer(self, name, manager):
         self.cursor.execute("insert into work.developers (id, name, manager) values(default,?,?)", 
             (name, manager))
-    
 
 
 def main():
@@ -95,6 +90,7 @@ def main():
     procedures = ReportingDeveloperProcedures(interface.cursor)
     procedures.view_all_rows('work.peer_reviews')
     procedures.add_peer_review(1, 1, 'good')
+
 
 if __name__ == "__main__":
     main()
