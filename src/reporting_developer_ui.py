c139f66ebcb6ae4c88f898fa5f979e80c9a924c7
Saylee <dharnesk@mail.uc.edu>
2018-03-24 13:19:54 -0400

    more cleaning

--- src/reporting_developer_ui.py
+++ src/reporting_developer_ui.py
@@ -43,46 +43,67 @@ class ReportingDeveloperUI:
     
     def configure_connection_form(self):
         print("")
-    
-        
+
 
 class ReportingDeveloperProcedures:
+
     def __init__(self, cursor):
         self.cursor = cursor
     
-    def view_all_rows(self, tablename):
-        self.cursor.execute("select item_id from "+tablename+";")
+    def view_all_rows(self, table_name):
+        self.cursor.execute("select item_id from " + table_name + ";")
         table = self.cursor.fetchall()
-        for row in self.cursor.columns(table=tablename):
-            print (row.column_name)
+        for row in self.cursor.columns(table=table_name):
+            print(row.column_name)
 
-    def add_business_review(self, item_id, approval, reviewer, comment, reviewed_datetime = datetime.datetime.now()):
+    def add_business_review(self, item_id, approval,
+                            reviewer, comment,
+                            reviewed_datetime=datetime.datetime.now()):
+
         reviewed = reviewed_datetime.strftime('%Y-%m-%d %H:%M:%S')
-        self.cursor.execute("insert into work.business_reviews (item_id, approval,reviewer,comment,reviewed)values(?,?,?,?,?)",
-            (item_id, approval, reviewer, comment, reviewed))
+        self.cursor.execute("insert into work.business_reviews "
+                            "(item_id, approval,reviewer,comment,reviewed)"
+                            "values(?,?,?,?,?)",
+                            (item_id, approval, reviewer, comment, reviewed))
     
-    def add_peer_review(self, item_id, approval, comment, reviewer= os.getlogin(), reviewed_datetime = datetime.datetime.now()):
+    def add_peer_review(self, item_id, approval,
+                        comment, reviewer=os.getlogin(),
+                        reviewed_datetime=datetime.datetime.now()):
         reviewed = reviewed_datetime.strftime('%Y-%m-%d %H:%M:%S')
-        self.cursor.execute("insert into work.peer_reviews (item_id, approval,reviewer,comment,reviewed)values(?,?,?,?,?)", 
-            (item_id, approval, reviewer, comment, reviewed))
+        self.cursor.execute("insert into work.peer_reviews "
+                            "(item_id, approval,reviewer,comment,reviewed)"
+                            "values(?,?,?,?,?)",
+                            (item_id, approval, reviewer, comment, reviewed))
 
-    def add_request_review(self, item_id, approval, comment, reviewer= os.getlogin(), reviewed_datetime = datetime.datetime.now()):
+    def add_request_review(self, item_id, approval,
+                           comment, reviewer=os.getlogin(),
+                           reviewed_datetime=datetime.datetime.now()):
         reviewed = reviewed_datetime.strftime('%Y-%m-%d %H:%M:%S')
-        self.cursor.execute("insert into request_reviews (item_id, approval,reviewer,comment,reviewed)values(?,?,?,?,?)", 
-            (item_id, approval, reviewer, comment, reviewed))
+        self.cursor.execute("insert into request_reviews "
+                            "(item_id, approval,reviewer,comment,reviewed)"
+                            "values(?,?,?,?,?)",
+                            (item_id, approval, reviewer, comment, reviewed))
 
     def add_note(self, item_id, note):
-        self.cursor.execute("insert into work.notes (item_id, note)values(?,?)", 
-            (item_id, note))
+        self.cursor.execute("insert into work.notes "
+                            "(item_id, note)"
+                            "values(?,?)",
+                            (item_id, note))
 
-    def add_level_of_effort(self, item_id, estimate, developer= os.getlogin(), added_datetime=datetime.datetime.now()):
+    def add_level_of_effort(self, item_id, estimate,
+                            developer=os.getlogin(),
+                            added_datetime=datetime.datetime.now()):
         added = added_datetime.strftime('%Y-%m-%d %H:%M:%S')
-        self.cursor.execute("insert into work.level_of_effort (item_id, estimate,developer, added)values(?,?,?,?)",
-            (item_id, estimate, developer, added))
+        self.cursor.execute("insert into work.level_of_effort "
+                            "(item_id, estimate,developer, added)"
+                            "values(?,?,?,?)",
+                            (item_id, estimate, developer, added))
 
     def add_developer(self, name, manager):
-        self.cursor.execute("insert into work.developers (id, name, manager) values(default,?,?)", 
-            (name, manager))
+        self.cursor.execute("insert into work.developers "
+                            "(id, name, manager) "
+                            "values(default,?,?)",
+                            (name, manager))
 
 
 def main():
@@ -94,5 +115,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
