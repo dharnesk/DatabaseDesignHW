453c66c45140f5ecddf44b89ef2b16b04cac13ec
Samuel Buzas <buzassg@mail.uc.edu>
2018-03-24 14:58:43 -0400

    Marlon Brando is a based god

---
+++ src/ApplicationMaster.py
@@ -0,0 +1,27 @@
+"""
+By Samuel Buzas
+Application UI
+"""
+
+import tkinter as tk
+
+class ApplicationUI(tk.Frame):
+    """
+    Documentation is my passion
+    """
+    def __init__(self, master=None):
+        """
+        Documentation is my passion
+        """
+        pass
+
+    def create_login(self):
+        """
+        Documentation is my passion
+        """
+        pass
+    def on_button_pressed(self):
+        """
+        Documentation is my passion
+        """
+        pass
