433e43da3eeb7a15b13f4fbd6972c5fd1715f43f
samwisebuze <buzassg@mail.uc.edu>
2018-03-27 15:37:30 -0400

    Added docstrings

--- src/reporting_developer_interface.py
+++ src/reporting_developer_interface.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import datetime
 import os
 import pyodbc
@@ -22,53 +23,95 @@ class ConfigInterface:
 
 
 class ReportingDeveloperFormProcedures:
+    """
+    Documentation is my passion
+    """
 
     def __init__(self, cursor):
         self.cursor = cursor
         self.helper = ReportingDeveloperHelperFunctions(cursor)
 
     def configuration_form_procedure(self):
+        """
+        Documentation is my passion.
+        """
         print("")
 
     def report_request_form_procedure(self):
+        """
+        Documentation is my passion.
+        """
         print("")
 
     def pending_review_form_procedure(self):
+        """
+        Documentation is my passion.
+        """
         print("")
 
     def assign_form_procedure(self):
+        """
+        Documentation is my passion.
+        """
         print("")
 
     def pending_development_form_procedure(self):
+        """
+        Documentation is my passion.
+        """
         print("")
 
     def development_form_procedure(self):
+        """
+        Documentation is my passion.
+        """
         print("")
 
     def peer_review_form_procedure(self):
+        """
+        Documentation is my passion.
+        """
         print("")
 
     def business_review_form_procedure(self, item_id, approval,
                                        reviewer, comment,
                                        reviewed_datetime):
+        """
+        Documentation is my passion.
+        """
         self.helper.add_business_review(item_id, approval,
-                                            reviewer, comment,
-                                            reviewed_datetime)
+                                        reviewer, comment,
+                                        reviewed_datetime)
         print("")
 
     def update_status_form_procedure(self):
+        """
+        Documentation is my passion.
+        """
         print("")
 
     def request_review_form_procedure(self):
+        """
+        Documentation is my passion.
+        """
         print("")
 
     def add_note_form_procedure(self):
+        """
+        Documentation is my passion.
+        """
         print("")
 
     def add_level_of_effort_form_procedure(self):
+        """
+        Documentation is my passion.
+        """
         print("")
 
     def add_developer_form_procedure(self):
+        """
+        Documentation is my passion.
+        """
         print("")
 
 
@@ -86,11 +129,14 @@ class ReportingDeveloperHelperFunctions:
         self.cursor.execute("select item_id from " + table_name + ";")
         table = self.cursor.fetchall()
         for row in self.cursor.columns(table=table_name):
-            print(row.column_name)    
+            print(row.column_name)
 
     def add_business_review(self, item_id, approval,
                             reviewer, comment,
                             reviewed_datetime=datetime.datetime.now()):
+        """
+        Documentation is my passion.
+        """
         reviewed = reviewed_datetime.strftime('%Y-%m-%d %H:%M:%S')
         self.cursor.execute("insert into work.business_reviews "
                             "(item_id, approval,reviewer,comment,reviewed)"
@@ -100,6 +146,9 @@ class ReportingDeveloperHelperFunctions:
     def add_peer_review(self, item_id, approval,
                         comment, reviewer=os.getlogin(),
                         reviewed_datetime=datetime.datetime.now()):
+        """
+        Documentation is my passion.
+        """
         reviewed = reviewed_datetime.strftime('%Y-%m-%d %H:%M:%S')
         self.cursor.execute("insert into work.peer_reviews "
                             "(item_id, approval,reviewer,comment,reviewed)"
@@ -109,6 +158,9 @@ class ReportingDeveloperHelperFunctions:
     def add_request_review(self, item_id, approval,
                            comment, reviewer=os.getlogin(),
                            reviewed_datetime=datetime.datetime.now()):
+        """
+        Documentation is my passion.
+        """
         reviewed = reviewed_datetime.strftime('%Y-%m-%d %H:%M:%S')
         self.cursor.execute("insert into request_reviews "
                             "(item_id, approval,reviewer,comment,reviewed)"
@@ -116,6 +168,9 @@ class ReportingDeveloperHelperFunctions:
                             (item_id, approval, reviewer, comment, reviewed))
 
     def add_note(self, item_id, note):
+        """
+        Documentation is my passion.
+        """
         self.cursor.execute("insert into work.notes "
                             "(item_id, note)"
                             "values(?,?)",
@@ -124,6 +179,9 @@ class ReportingDeveloperHelperFunctions:
     def add_level_of_effort(self, item_id, estimate,
                             developer=os.getlogin(),
                             added_datetime=datetime.datetime.now()):
+        """
+        Documentation is my passion.
+        """
         added = added_datetime.strftime('%Y-%m-%d %H:%M:%S')
         self.cursor.execute("insert into work.level_of_effort "
                             "(item_id, estimate,developer, added)"
@@ -131,6 +189,9 @@ class ReportingDeveloperHelperFunctions:
                             (item_id, estimate, developer, added))
 
     def add_developer(self, name, manager):
+        """
+        Documentation is my passion.
+        """
         self.cursor.execute("insert into work.developers "
                             "(id, name, manager) "
                             "values(default,?,?)",
@@ -138,8 +199,11 @@ class ReportingDeveloperHelperFunctions:
 
 
 def main():
+    """
+    Documentation is my passion.
+    """
     interface = ConfigInterface()
-    procedures = ReportingDeveloperProcedures(interface.cursor)
+    procedures = ReportingDeveloperFormProcedures(interface.cursor)
     procedures.view_all_rows('work.peer_reviews')
     # procedures.add_peer_review(1, 1, 'good')
 
