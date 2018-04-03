f9a76ee41ee9e7c6a72e7973d5cd30d6e516aebe
Saylee <dharnesk@mail.uc.edu>
2018-04-03 17:32:32 -0400

    mapped functions to different forms

--- src/reporting_developer_interface.py
+++ src/reporting_developer_interface.py
@@ -30,51 +30,134 @@ class ReportingDeveloperFormProcedures:
     def configuration_form_procedure(self):
         print("")
 
-    def report_request_form_procedure(self):
-        self.helper.add_item()
-        self.helper.add_request_review()
+    def report_request_form_procedure(self, name, requester, department, priority, purpose, requirements, work_type):
+        """
+        :param name: string name of item
+        :param requester: name of item requester
+        :param department: department of request
+        :param priority: priority
+        :param purpose: text about purpose of item
+        :param requirements: text about requirements
+        :param work_type: fk type of work
+        :return: none
 
-    def pending_review_form_procedure(self):
-        self.helper.change_review_pending_request()
-        self.helper.update_status()
+        """
+        self.helper.add_item(name, requester, department, priority, purpose, requirements, work_type)
 
-    def assign_form_procedure(self):
-        self.helper.assign_item()
-        self.helper.update_status()
+    def pending_review_form_procedure(self, item_id, approval, comment, reviewed, reviewer):
+        """
+        :param item_id: int fk item id
+        :param approval: int 0 or 1 for approval
+        :param comment: text for comment
+        :param reviewed: datetime of when reviewed
+        :param reviewer: fk varchar(30) username of reviewer
+        :return: none
+        """
+        self.helper.add_request_review(item_id, approval, comment, reviewed, reviewer)
 
-    def pending_development_form_procedure(self):
-        self.helper.add_developer_review()
-        self.helper.update_status()
+    def assign_form_procedure(self, item_id, assignee, assigned=datetime.datetime.now(), assigner=None):
+        """
+        :param item_id: fk int item id
+        :param assignee: fk varchar(30) username of assignee
+        :param assigned: datetime of when assigned
+        :param assigner: fk varchar(30) username of assigner
+        :return: none
+        """
+        self.helper.assign_item(item_id, assignee, assigned, assigner)
+        self.helper.update_status(item_id, 'Assigned')
 
-    def development_form_procedure(self):
-        self.helper.update_status()
+    def pending_development_form_procedure(self, item_id, estimate, comment, reviewer, est_delivery=None,
+                                           added=datetime.datetime.now()):
+        """
+        :param item_id: fk int from work.items column
+        :param estimate: int estimated number of hours
+        :param comment: text for comments
+        :param reviewer: name of reviewer
+        :param est_delivery: datetime estimated delivery time
+        :param added: datetime added
+        :return: none
+        """
+        self.helper.add_developer_review(item_id, estimate, comment, reviewer, est_delivery, added)
+        self.helper.update_status(item_id, 'Pending Development')
 
-    def peer_review_form_procedure(self):
-        self.helper.add_peer_review()
-        self.helper.update_status()
+    def development_form_procedure(self, item_id):
+        """
+        :param item_id: fk int from work.items column
+        :return: none
+        """
+        self.helper.update_status(item_id, 'Development')
+
+    def peer_review_form_procedure(self, item_id, approval, comment, reviewed=datetime.datetime.now(), reviewer=None):
+        """
+        :param item_id: fk int item id
+        :param approval: int 0 or 1
+        :param comment: text for comment
+        :param reviewed: datetime when reviewed
+        :param reviewer: fk fk varchar(30) reviewer username
+        :return: none
+        """
+        self.helper.add_peer_review(item_id, approval, comment, reviewed, reviewer)
+        self.helper.update_status(item_id, 'Peer Review')
 
     def business_review_form_procedure(self, item_id, approval,
                                        reviewer, comment,
                                        reviewed_datetime):
-        self.helper.add_business_review(item_id, approval,
-                                            reviewer, comment,
-                                            reviewed_datetime)
-        self.helper.update_status()
+        """
+        :param item_id: fk int from work.items column
+        :param approval: 0 or 1 integer
+        :param reviewer: string name of reviewer
+        :param comment: string comment
+        :param reviewed: datetime of when reviewed
+        :return: none
+        """
+        self.helper.update_status(item_id, 'Business Review')
+        self.helper.add_business_review(item_id, approval, reviewer, comment, reviewed_datetime)
 
-    def update_status_form_procedure(self):
-        self.helper.update_status()
+    def update_status_form_procedure(self, item_id, next_status):
+        """
+        :param item_id: fk int from work.items column
+        :param next_status: next status
+        :return: none
+        """
+        # NEXT Status can be Closed or On Hold
+        self.helper.update_status(item_id, next_status)
 
-    def request_review_form_procedure(self):
-        self.helper.add_request_review()
+    def request_review_form_procedure(self, item_id, approval, comment, reviewed=datetime.datetime.now()):
+        """
+        :param item_id: int fk item id
+        :param approval: int 0 or 1 approval
+        :param comment: text with comments
+        :param reviewed: datetime of when reviewed
+        :return: none
+        """
+        self.helper.change_review_pending_request(item_id, approval, comment, reviewed)
+        self.helper.update_status(item_id, 'Request Reviewed')
 
-    def add_note_form_procedure(self):
-        self.helper.add_note()
+    def add_note_form_procedure(self, item_id, note):
+        """
+        :param item_id: int fk item id
+        :param note: text for note
+        :return: none
+        """
+        self.helper.add_note(item_id, note)
 
-    def add_level_of_effort_form_procedure(self):
-        self.helper.add_level_of_effort()
+    def add_level_of_effort_form_procedure(self, item_id, estimate, added=datetime.datetime.now(), developer=None):
+        """
+        :param item_id: fk int from work.items column
+        :param estimate: int estimated number of hours
+        :param added: datetime of when level of effort was added
+        :param developer: string(30 characters) username
+        :return: none
+        """
+        self.helper.add_level_of_effort(item_id, estimate, added, developer)
 
-    def add_developer_form_procedure(self):
-        self.helper.add_developer()
+    def add_developer_form_procedure(self, name, manager):
+        """
+        :param name: varchar(128) name of developer
+        :param manager: varchar(100) name of manager
+        :return: none
+        """
+        self.helper.add_developer(name, manager)
 
 
 class ReportingDeveloperHelperFunctions:
@@ -88,6 +171,41 @@ class ReportingDeveloperHelperFunctions:
         """
         self.cursor.execute('SELECT SUSER_SNAME()')
         return (self.cursor.fetchall()[0][0])[0:30]
+
+    def add_item(self, name, requester, department, priority, purpose, requirements, work_type):
+        """
+        :param name: string name of item
+        :param requester: name of item requester
+        :param department: department of request
+        :param priority: priority
+        :param purpose: text about purpose of item
+        :param requirements: text about requirements
+        :param work_type: fk type of work
+        :return: none
+        """
+        added_by = self.get_truncated_username()
+        requested = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        self.cursor.execute("""insert into work.items (name, requested, added_by, requestor, department, priority, 
+                            purpose,requirements, type, request_location)
+                            values ('{}','{}', '{}', '{}','{}','{}','{}','{}', '{}', '{}')""".format(
+                            name, requested, added_by,  requester, department, priority,
+                            purpose, requirements, work_type, 'Internal'))
+
+    def add_request_review(self, item_id, approval, comment, reviewed=datetime.datetime.now(), reviewer=None):
+        """
+        :param item_id: fk int item id
+        :param approval: int 0 or 1 for approval
+        :param comment: text for comment
+        :param reviewed: datetime of when reviewed
+        :param reviewer: fk varchar(30) username of reviewer
+        :return: none
+        """
+        reviewed = reviewed.strftime('%Y-%m-%d %H:%M:%S')
+        if reviewer is None:
+            reviewer = self.get_truncated_username()
+        self.cursor.execute("""INSERT INTO work.request_reviews (item_id, approval, comment, reviewer, reviewed)
+                            VALUES('{}','{}','{}','{}','{}')""".format
+                            (item_id, approval, comment, reviewer, reviewed))
 
     def add_business_review(self, item_id, approval, reviewer, comment, reviewed=datetime.datetime.now()):
         """
@@ -146,25 +264,6 @@ class ReportingDeveloperHelperFunctions:
                             values('{}','{}','{}','{}','{}','{}')""". format(
                             item_id, estimate, comment, reviewer, added, est_delivery))
 
-    def add_item(self, name, requester, department, priority, purpose, requirements, work_type):
-        """
-        :param name: string name of item
-        :param requester: name of item requester
-        :param department: department of request
-        :param priority: priority
-        :param purpose: text about purpose of item
-        :param requirements: text about requirements
-        :param work_type: fk type of work
-        :return: none
-        """
-        added_by = self.get_truncated_username()
-        requested = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        self.cursor.execute("""insert into work.items (name, requested, added_by, requestor, department, priority, 
-                            purpose,requirements, type, request_location)
-                            values ('{}','{}', '{}', '{}','{}','{}','{}','{}', '{}', '{}')""".format(
-                            name, requested, added_by,  requester, department, priority,
-                            purpose, requirements, work_type, 'Internal'))
-
     def add_note(self, item_id, note):
         """
         :param item_id: fk int item id
@@ -188,22 +287,6 @@ class ReportingDeveloperHelperFunctions:
         if reviewer is None:
             reviewer = self.get_truncated_username()
         self.cursor.execute("""INSERT INTO work.peer_reviews (item_id, approval, comment, reviewer, reviewed) 
-                            VALUES('{}','{}','{}','{}','{}')""".format
-                            (item_id, approval, comment, reviewer, reviewed))
-
-    def add_request_review(self, item_id, approval, comment, reviewed=datetime.datetime.now(), reviewer=None):
-        """
-        :param item_id: fk int item id
-        :param approval: int 0 or 1 for approval
-        :param comment: text for comment
-        :param reviewed: datetime of when reviewed
-        :param reviewer: fk varchar(30) username of reviewer
-        :return:
-        """
-        reviewed = reviewed.strftime('%Y-%m-%d %H:%M:%S')
-        if reviewer is None:
-            reviewer = self.get_truncated_username()
-        self.cursor.execute("""INSERT INTO work.request_reviews (item_id, approval, comment, reviewer, reviewed)
                             VALUES('{}','{}','{}','{}','{}')""".format
                             (item_id, approval, comment, reviewer, reviewed))
 
