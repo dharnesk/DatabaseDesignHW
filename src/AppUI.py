40b72ab03263ef9172da261ed2b8aceeba2d5f31
Samwisebuze <buzassg@mail.uc.edu>
2018-04-02 22:07:47 -0400

    Updating Form docstrings

--- src/AppUI.py
+++ src/AppUI.py
@@ -1,4 +1,8 @@
-
+"""
+Application UI
+This Module handles the application's User Interface.
+This is done using mutiple forms in a Tkinter Notebook.
+"""
 from __future__ import print_function
 import datetime
 import os
@@ -110,10 +114,10 @@ class AppUI(tk.Tk):
         container.grid_columnconfigure(0, weight=1)
         self.server_name = ""
         self.winfo_toplevel().title("Reporting Developer GUI")
-
+        # Initialize the Notebook and all of its Forms
         notebook = ttk.Notebook(container)
         notebook.add(ConfigurationPage(self), text='Configuration')
-        notebook.add(FormOne(self), text="Form One")
+        notebook.add(FormOne(self), text="Report Request Input Form")
         notebook.add(FormTwo(self), text="Form Two")
         notebook.add(FormThree(self), text="Form Three")
         notebook.add(FormFour(self), text="Form Four")
@@ -139,16 +143,21 @@ class ConfigurationPage(ttk.Frame):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Configuration Page")
         label.pack(pady=10)
+
         label2 = tk.Label(self, text="Server Name")
         label2.pack()
+
         e = tk.Entry(self)
         e.pack()
         e.focus_set()
+
         label3 = tk.Label(self, text="Driver")
         label3.pack()
+
         e2 = tk.Entry(self)
         e2.pack()
         e2.focus_set()
+
         label_success = tk.Label(self, text="Connection Successful")
         label_failed = tk.Label(self, text="Connection Failed")
         button = tk.Button(self,
@@ -164,13 +173,28 @@ class ConfigurationPage(ttk.Frame):
 
 
 class FormOne(ttk.Frame):
+    """
+    :param item_id:
+    :param:
+    :param:
+    :param:
+    :param:
+    """
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
+        #Form Title
         label = tk.Label(self, text="Report Request Input Form")
         label.pack(pady=10)
-        e = tk.Entry(self)
-        e.pack()
-        e.focus_set()
+        # Text Entry Field Title
+        label2 = tk.Label(self, text="Text Entry Field Title")
+        label2.pack()
+
+        # Text Entry Field
+        field1 = tk.Entry(self)
+        field1.pack()
+        field1.focus_set()
+
+        # Submit Button
         button = tk.Button(self,
                            text="Submit",
                            fg="red",
@@ -183,9 +207,11 @@ class FormTwo(ttk.Frame):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Pending Review Input Form")
         label.pack()
+
         e = tk.Entry(self)
         e.pack()
         e.focus_set()
+        # Submit Button
         button = tk.Button(self,
                            text="Submit",
                            fg="red",
@@ -196,11 +222,14 @@ class FormTwo(ttk.Frame):
 class FormThree(ttk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
+        #Form Title
         label = tk.Label(self, text="Assigned Input Form")
         label.pack()
+
         e = tk.Entry(self)
         e.pack()
         e.focus_set()
+        # Submit Button
         button = tk.Button(self,
                            text="Submit",
                            fg="red",
@@ -212,11 +241,14 @@ class FormThree(ttk.Frame):
 class FormFour(ttk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
+
         label = tk.Label(self, text="Pending Development Input Form")
         label.pack()
+
         e = tk.Entry(self)
         e.pack()
         e.focus_set()
+        #Submit Button
         button = tk.Button(self,
                            text="Submit",
                            fg="red",
@@ -227,11 +259,14 @@ class FormFour(ttk.Frame):
 class FormFive(ttk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
+
         label = tk.Label(self, text="Development Input Form")
         label.pack()
+
         e = tk.Entry(self)
         e.pack()
         e.focus_set()
+        #Submit Button
         button = tk.Button(self,
                            text="Submit",
                            fg="red",
@@ -242,11 +277,14 @@ class FormFive(ttk.Frame):
 class FormSix(ttk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
+
         label = tk.Label(self, text="Peer Review Input Form")
         label.pack()
+
         e = tk.Entry(self)
         e.pack()
         e.focus_set()
+        #Submit button
         button = tk.Button(self,
                            text="Submit",
                            fg="red",
@@ -287,7 +325,7 @@ class BusinessReviewInputForm(ttk.Frame):
         comment = tk.Entry(self)
         comment.pack()
         comment.focus_set()
-
+        #Submit button
         button = tk.Button(self,
                            text="Submit",
                            fg="red",
@@ -301,11 +339,14 @@ class BusinessReviewInputForm(ttk.Frame):
 class FormEight(ttk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
+
         label = tk.Label(self, text="Update Status Input Form")
         label.pack()
+
         e = tk.Entry(self)
         e.pack()
         e.focus_set()
+
         button = tk.Button(self,
                            text="Submit",
                            fg="red",
@@ -313,13 +354,28 @@ class FormEight(ttk.Frame):
         button.pack(pady=10)
 
 class FormNine(ttk.Frame):
+    """
+    :param int item_id:
+        forigen key
+    :param bool approval:
+    :param string comment:
+        No limit
+    :param datetime reviewed:
+        Defualt = current time, nullable 
+    :param string reviewer:
+        30 character limit,
+        default = current user, nullable
+    """
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
+
         label = tk.Label(self, text="Request Review Input Form")
         label.pack()
+
         e = tk.Entry(self)
         e.pack()
         e.focus_set()
+        # Submit Button
         button = tk.Button(self,
                            text="Submit",
                            fg="red",
@@ -327,6 +383,10 @@ class FormNine(ttk.Frame):
         button.pack(pady=10)
 
 class FormTen(ttk.Frame):
+    """
+    :param int item_id:
+    :param string comment: No limit
+    """
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Add Note Input Form")
@@ -341,10 +401,23 @@ class FormTen(ttk.Frame):
         button.pack(pady=10)
 
 class FormEleven(ttk.Frame):
+    """
+    :param int item_id:
+    :param int estimate: Work hour estimate 8h = 1 day 40h=1w
+    :param datetime add_date:
+        Can be null
+        When the level_of_effort was added, default = CurrTime
+    :param string developer:  
+        Can be null
+        30 characters, restricted
+        default = username of person
+    """
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
+
         label = tk.Label(self, text="Add Level of Effort Input Form")
         label.pack()
+
         e = tk.Entry(self)
         e.pack()
         e.focus_set()
@@ -355,13 +428,19 @@ class FormEleven(ttk.Frame):
         button.pack(pady=10)
 
 class FormTwelve(ttk.Frame):
+    """
+    TODO: Needs Params
+    """
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
+
         label = tk.Label(self, text="Add a Developer Input Form")
         label.pack()
+
         e = tk.Entry(self)
         e.pack()
         e.focus_set()
+        #Submit Button
         button = tk.Button(self,
                            text="Submit",
                            fg="red",
